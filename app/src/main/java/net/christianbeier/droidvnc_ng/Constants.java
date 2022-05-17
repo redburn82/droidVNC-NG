cf7b9e35048a6b3d2fa18ca79a1de0ebedd906b5
redburn82 <redburn82@gmail.com>
2022-05-17 13:31:33 +0900

    Update Constants.java

--- app/src/main/java/net/christianbeier/droidvnc_ng/Constants.java
+++ app/src/main/java/net/christianbeier/droidvnc_ng/Constants.java
@@ -27,7 +27,7 @@ public class Constants {
     public static final int DEFAULT_PORT_REVERSE = 5500;
     public static final float DEFAULT_SCALING = 0.35f; //JHG
     public static final int DEFAULT_PORT_REPEATER = 5500;
-    #public static final float DEFAULT_SCALING = 1.0f;
+    //public static final float DEFAULT_SCALING = 1.0f;
     public static final String PREFS_KEY_SETTINGS_PORT = "settings_port";
     public static final String PREFS_KEY_SETTINGS_PASSWORD = "settings_password" ;
     public static final String PREFS_KEY_SETTINGS_START_ON_BOOT = "settings_start_on_boot" ;
